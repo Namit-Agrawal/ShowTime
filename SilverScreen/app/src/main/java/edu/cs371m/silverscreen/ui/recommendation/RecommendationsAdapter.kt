c36794b270ea300e9f441248a01783ce21c88880
Lucinda Nguyen <lucinda@cs.utexas.edu>
2019-12-03 22:00:59 -0600

    started recommendations

---
+++ SilverScreen/app/src/main/java/edu/cs371m/silverscreen/ui/recommendation/RecommendationsAdapter.kt
@@ -0,0 +1,73 @@
+package edu.cs371m.silverscreen.ui.recommendation
+
+import android.text.style.TtsSpan
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.recyclerview.widget.RecyclerView
+import edu.cs371m.silverscreen.R
+import edu.cs371m.silverscreen.api.api.MovieDBPost
+import edu.cs371m.silverscreen.api.api.MoviePost
+import edu.cs371m.silverscreen.ui.movies.MoviesViewModel
+
+class RecommendationsAdapter(private val viewModel: MoviesViewModel): RecyclerView.Adapter<RecommendationsAdapter.VH>() {
+    private var recommendations = listOf<MovieDBPost>()
+
+    inner class VH(itemView: View): RecyclerView.ViewHolder(itemView){
+        var movieTitleText = itemView.findViewById<TextView>(R.id.movie_name)
+        var durationText = itemView.findViewById<TextView>(R.id.duration)
+        var actors = itemView.findViewById<TextView>(R.id.pop_actors)
+        var date = itemView.findViewById<TextView>(R.id.date)
+        var thumbnail = itemView.findViewById<ImageView>(R.id.thumbnail)
+        var wholePost = itemView.findViewById<LinearLayout>(R.id.post)
+
+        var fav = itemView.findViewById<ImageButton>(R.id.favBut)
+
+        init{
+
+        }
+        fun bind(item: MovieDBPost?) {
+            if (item == null) {
+                return;
+            }
+            movieTitleText.text = item.movieName
+            date.text = item.date
+//            MovieViewModel.netCreditRefresh(item.movie_id)
+//            MovieViewModel.netMovieRefresh(item.movie_id)
+            // val cast = MovieViewModel.observeCast().observe(fragment, Observer {  })
+
+            // durationText.text = movie?.duration.toString() + "minutes"
+
+//            val cast_array = cast!!.cast
+//            actors.text = cast_array[0].actor +", " +cast_array[1].actor
+            if(item.thumbnail!=null)
+            {
+                viewModel.netFetchImage(item.thumbnail, thumbnail, false)
+            }
+
+
+        }
+
+
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+
+            holder.bind(recommendations[holder.adapterPosition])
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.movie_row, parent, false)
+        return VH(itemView)
+    }
+    override fun getItemCount(): Int {
+        return recommendations.size
+    }
+    fun submitList(items: List<MovieDBPost>) {
+        recommendations = items
+        notifyDataSetChanged()
+    }
+
+}
--- SilverScreen/app/src/main/java/edu/cs371m/silverscreen/ui/recommendation/RecommendationsFragment.kt
+++ SilverScreen/app/src/main/java/edu/cs371m/silverscreen/ui/recommendation/RecommendationsFragment.kt
@@ -6,31 +6,51 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import edu.cs371m.silverscreen.R
+import edu.cs371m.silverscreen.ui.movies.MovieRowAdapter
+import edu.cs371m.silverscreen.ui.movies.MoviesViewModel
 
 class RecommendationsFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: RecommendationsViewModel
-
+    private lateinit var viewModel: MoviesViewModel
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProviders.of(this).get(RecommendationsViewModel::class.java)
+        viewModel =
+            ViewModelProviders.of(this).get(MoviesViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_recommendations, container, false)
         if(activity is AppCompatActivity){
             val act = (activity as AppCompatActivity).supportActionBar
             act?.hide()
         }
-        val textView: TextView = root.findViewById(R.id.text_recommendations)
-        dashboardViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+
+
+
+        val adapter = initRecyclerView(root)
+//        viewModel.observeMovies().observe(this, Observer {
+//            adapter.submitList(it)
+//        })
         return root
     }
-}
+
+
+    private fun initRecyclerView(root: View?): RecommendationsAdapter {
+        val rv = root!!.findViewById<RecyclerView>(R.id.recycler_view_movies)
+
+        val adapter =RecommendationsAdapter(viewModel)
+        rv.adapter = adapter
+        rv.layoutManager = LinearLayoutManager(context)
+        val itemDecor = DividerItemDecoration(rv.context, LinearLayoutManager.VERTICAL)
+        itemDecor.setDrawable(ContextCompat.getDrawable(rv.context, R.drawable.divider)!!)
+        rv.addItemDecoration(itemDecor)
+        return adapter
+    }}
